862b050908ac0c9bcfac0620c1c7ec9de4dc3efe
Garnn <bawalilko@gmail.com>
2023-12-18 18:18:16 +0100

    Initial movement implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "SFML/Graphics/Transform.hpp"
+#include "SFML/Window/Event.hpp"
+#include "SFML/Window/Keyboard.hpp"
 #include <SFML/Graphics.hpp>
 #include <math.h>
 
@@ -7,29 +9,44 @@ int main()
     auto window = sf::RenderWindow{ { 200u, 200u }, "ReturnPong" };
     window.setVerticalSyncEnabled(true);
 
-    sf::CircleShape circle(5.f);
-    circle.setFillColor(sf::Color::White);
+    sf::CircleShape player(5.f);
+    player.setFillColor(sf::Color::White);
     float rotation = 0;
-    circle.setPosition(window.getSize().x/2.f,window.getSize().y/2.f);
+    player.setPosition(window.getSize().x/2.f,window.getSize().y/2.f);
 
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
+
+
             if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
             
+            
         }
-        rotation+=0.1f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                player.move(0,-2);
+        }
 
-        circle.setPosition(window.getSize().x/2.f+sin(rotation)*50.f,window.getSize().y/2.f+cos(rotation)*20.f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                player.move(0,2);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                player.move(-2,0);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                player.move(2,0);
+        }
 
 
         window.clear(sf::Color::Black);
 
-        window.draw(circle);
+        window.draw(player);
         window.display();
     }
 }
