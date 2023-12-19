34a9dfe86f507de977ac637eca07c6357b7c4b85
Garnn <bawalilko@gmail.com>
2023-12-19 11:22:56 +0100

    Fixed tracking

--- src/balls.cpp
+++ src/balls.cpp
@@ -4,17 +4,22 @@
 #include "SFML/System/Vector2.hpp"
 #include <cmath>
 #include <iostream>
-class BlackBall{
-    private:
+#include <stdio.h>
+
+class Ball{
+    protected:
         sf::Vector2f _position;
         float _direction, _speed;
         float _lowerXLim, _upperXLim, _lowerYLim, _upperYLim;
 
-        void constrain_direction(){
-            _direction = fmod(_direction,360);
-            if(_direction<0) _direction+=360;
+        //Ograniczanie kątów
+        float constrain_angle(float angle){
+            angle = fmod(angle,360);
+            if(angle<0) angle+=360;
+            return angle;
         }
 
+        //Sprawdzenie czy kulka jest na ekranie
         void check_bounds(){
             if (_position.x>_upperXLim) {
                 _direction+=90;
@@ -36,24 +41,58 @@ class BlackBall{
     public:
         sf::CircleShape circ;
 
-        BlackBall(sf::Vector2f position, float direction, float speed, int lowerXLim, int upperXLim, int lowerYLim, int upperYLim){
+        //Konstruktor
+        Ball(sf::Vector2f position, float direction, float speed, int lowerXLim, int upperXLim, int lowerYLim, int upperYLim){
             _position = position;
             _direction = direction;
             _speed = speed;
             circ = sf::CircleShape(5.f);
-            circ.setFillColor(sf::Color::Black);
             _lowerXLim = lowerXLim;
             _upperXLim = upperXLim;
             _lowerYLim = lowerYLim;
             _upperYLim = upperYLim;
-        }
+        };
         
+        //Obsługa ruchu
         void move(){
             check_bounds();
 
-            constrain_direction();
+            _direction = constrain_angle(_direction);
             _position.x+=cos(_direction*(M_PI/180))*_speed;
             _position.y+=sin(_direction*(M_PI/180))*_speed;
             circ.setPosition(_position);
+        }
+};
+
+class CurveBall : public Ball{
+
+    using Ball::Ball;
+    public:
+        //Skręcanie
+        void skew(){
+            _direction+=0.2f;
+        }
+
+};
+
+class TrackingBall : public Ball{
+
+    using Ball::Ball;
+    public:
+        void skew_towards(sf::Vector2f target){
+
+            float futureX = _position.x+cos(_direction*(M_PI/180))*_speed;
+            float futureY = _position.y+sin(_direction*(M_PI/180))*_speed;
+
+            //Iloczyn wektorowy aby określić czy kulka jest po lewej czy prawej stronie, aby wiedzieć jak skręcać
+            float crossp = (futureX - _position.x)*(target.y-_position.y)-(futureY-_position.y)*(target.x-_position.x);
+
+            if(crossp>0){
+                _direction+=1.f;
+            }
+            if(crossp<0){
+                _direction-=1.f;
+            }
+
         }
 };
